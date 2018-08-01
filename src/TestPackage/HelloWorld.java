bcab275ddcf428437a2dd45b30ca6582e593ea74
Artem <chilly.artem@gmail.com>
2018-08-01 09:58:13 +0300

    First commit

---
+++ src/TestPackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+/**
+ * Created by Artem_Berdnik on 8/1/2018.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
