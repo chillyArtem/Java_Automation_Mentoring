8f3737066d38c26b235bf1360fcee0341dfa8599
Artem <chilly.artem@gmail.com>
2018-08-15 14:29:04 +0300

    done notReplace task

--- src/main/java/CodingBat/String3/notReplace.java
+++ src/main/java/CodingBat/String3/notReplace.java
@@ -1,29 +1,42 @@
 package CodingBat.String3;
 
-import java.util.Arrays;
-import java.util.List;
-
-/**@SuppressWarnings
- * Created by Artem_Berdnik on 8/1/2018.
+/**
+ * @SuppressWarnings Created by Artem_Berdnik on 8/1/2018.
  */
 
-//FIXME is not working
 public class notReplace {
     public static void main(String[] args) {
-        System.out.println(notReplace("is-is"));
+        System.out.println(notReplace("is"));
     }
 
     public static String notReplace(String str) {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder(str);
 
-        for (int i = 0; i < str.length()-1; i++) {
-            if (str.substring(i, i + 2).equals("is") && !Character.isLetter(str.charAt(i-1)) && !Character.isLetter(str.charAt(i-2+1))){
-                result.insert(0, str.substring(0, i + 2)).append(" not").append(str.substring(i + 2));
+        for (int i = 0; i < result.length() - 1; i++) {
+            if (result.substring(i, i + 2).equals("is")) {
+                if (i - 1 >= 0 && i + 2 < result.length()) {
+                    if (!Character.isLetter(result.charAt(i - 1)) && !Character.isLetter(result.charAt(i + 2))) {
+                        result.replace(i, i + 2, "is not");
+                    }
+                }
+                if (i - 1 < 0 && i + 2 >= result.length()) {
+                        result.replace(i, i + 2, "is not");
+                        break;
+                }
+                if (i - 1 < 0) {
+                    if (!Character.isLetter(result.charAt(i + 2))) {
+                        result.replace(i, i + 2, "is not");
+                    }
+                }
+                if (i + 2 >= result.length()) {
+                    if (!Character.isLetter(result.charAt(i - 1))) {
+                        result.replace(i, i + 2, "is not");
+                    }
                 }
             }
-
-
-        return result.toString();
         }
 
+        return result.toString();
     }
+
+}
